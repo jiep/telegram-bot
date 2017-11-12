a449d512c8475f71298a4b25fe48ad9987074bb6
José Ignacio Escribano <jiep12345@hotmail.com>
2017-11-12 16:26:11 +0100

    Fixed errors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var log = colors.green;
 const path = process.env.FILES_PATH
 const chatId = process.env.CHAT_ID
 const bot = new Telegraf(process.env.BOT_TOKEN)
-const folders = ["rain_princess", "wreck", "la_muse", "udnie", "wave", "scream"]
+const folders = ["originales", "rain_princess", "wreck", "la_muse", "udnie", "wave", "scream"]
 
 photos = []
 
@@ -28,39 +28,24 @@ folders.forEach(folder => {
 	})
 })
 
-console.log(`------------------------------
-			| FICHEROS                     |
-			------------------------------`);
+console.log(
+	`------------------------------
+	| FICHEROS                     |
+	------------------------------`);
 photos.forEach(photo => {
 	console.log(photo.filePath);
 })
 console.log("Fotos totales: " + photos.length)
 
-/*
-bot.command('/photos', (ctx) => {
-	photos.forEach(photo => {
-		console.log(log(`Photo: ${photo}`))
-		ctx.replyWithPhoto({ source: photo.filePath }, {caption: photo.caption})
-			.then((response) => console.log("OK"))
-			.catch((error) => console.error("Error"));
-		//sleep.sleep(10);
-	})
-	
-})
-*/
-
-bot.command('/prueba', async (ctx) => {
+bot.command('/photos', async (ctx) => {
 	for(const [i, photo] of photos.entries()){
 		try {
 			await ctx.replyWithPhoto({ source: photo.filePath }, {caption: `${photo.caption} ${photo.name}`})
 			console.log(log(`${i}: ${photo.filePath}`));
 		} catch (err) {
-			console.err(error(`${i}: ${photo.filePath}`))
+			console.log(error(`${i}: ${photo.filePath}`))
 		}
 	}
-		
-
-	
 })
 
 
